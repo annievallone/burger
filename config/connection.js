6f9c389673fad5f39a5c27ae065c3b39de1bad2d
Annie Vallone <annievallone@gmail.com>
2018-08-23 18:22:17 -0400

    blah

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,5 @@
 var mysql = require("mysql");
 var connection;
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 8889,
-//   user: "root",
-//   password: "root",
-//   database: "burgers_db"
-// });
-
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
